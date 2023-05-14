565d0f19796e4c904480562706676adf3e23f563
Leonel <leonelsanz15@gmail.com>
2023-05-14 19:33:01 -0300

    agrego config cors

--- src/main/java/com/portfolio/leo/LeoApplication.java
+++ src/main/java/com/portfolio/leo/LeoApplication.java
@@ -2,6 +2,8 @@ package com.portfolio.leo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LeoApplication {
@@ -10,4 +12,12 @@ public class LeoApplication {
 		SpringApplication.run(LeoApplication.class, args);
 	}
 
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://protfolio-argprog.web.app", "https://portfolio-backend-argprog.onrender.com").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
